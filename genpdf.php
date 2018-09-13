e09893e54e7b2782f947a054fc13c915b62ff9d0
JJ <jj@kurrency.com>
2018-09-13 14:22:46 +0800

    all

---
+++ genpdf.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+
+$html = file_get_contents('sample.html');
+$dompdf->loadHtml($html);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'portrait');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+
+
+?>
